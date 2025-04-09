bedcaacfcc59f45f0e2f8d7003475bf569d32489
Rohitjiagarwal <138634457+Rohitjiagarwal@users.noreply.github.com>
2025-04-09 17:42:56 +0530

    fixed

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // Keep this for static site generation
-  images: {
-    unoptimized: true, // Disable Image Optimization API
-  },
+  
 };
 
 module.exports = nextConfig;
