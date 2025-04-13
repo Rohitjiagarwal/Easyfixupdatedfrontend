1e79f3b9e6d5c655ef900aed667024b40e4c6429
Easyfix <easyfixexpert@gmail.com>
2025-04-13 17:52:39 +0530

    favicon

--- pages/[city]/[brand]/[product].tsx
+++ pages/[city]/[brand]/[product].tsx
@@ -141,6 +141,25 @@ export default function DetailPage({
         {/* Robots Meta Tag */}
         <meta name="robots" content="index, follow" />
 
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/album/favicon//apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/album/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/album/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+
         {/* Structured Data (JSON-LD) for Service, Ratings, Address, and Mobile Number */}
         <script
           type="application/ld+json"
